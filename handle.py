a676bfec47fac91d468d60bcb3bb9375773cdbbe
elkun1 <kun@stanford.edu>
2018-04-07 23:01:13 -0400

    Testing first effort

--- handle.py
+++ handle.py
@@ -1,33 +1,24 @@
 # -*- coding: utf-8 -*-
 # filename: handle.py
-
 import hashlib
+import reply
+import receive
 import web
 
 class Handle(object):
-    def GET(self):
+    def POST(self):
         try:
-            data = web.input()
-            if len(data) == 0:
-                return "hello, this is handle view"
-            signature = data.signature
-            timestamp = data.timestamp
-            nonce = data.nonce
-            echostr = data.echostr
-            token = "hellobqlq"
-
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
-            print "handle/GET func: hashcode, signature: ", hashcode, signature
-            if hashcode == signature:
-                return echostr
+            webData = web.data()
+            print ("Handle Post webdata is ", webData)   #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = "test"
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
             else:
-                return ""
-        except Exception, Argument:
-            return Argument
-
-
-#just checking
+                print ("Processing Temporarily Suspended")
+                return "success"
+        except Exception:
+            return "Exception occurred"
