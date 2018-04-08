a21363087d54805021c30f2f68863a2546684c92
elkun1 <kun@stanford.edu>
2018-04-07 23:43:53 -0400

    new

--- handle.py
+++ handle.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+# filename: main.py
+import web
+
+urls = (
+    '/wx', 'Handle',
+)
+
+class Handle(object):
+    def GET(self):
+        return "hello, this is a test"
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
+    
+"""
 class Handle(object):
     def GET(self):
         try:
@@ -37,3 +54,4 @@ class Handle(object):
                 return "success"
         except Exception, Argment:
             return Argment
+"""
