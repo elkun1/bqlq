daf4c930af9d69f38d31691efe7c1453ad07cc8b
elkun1 <kun@stanford.edu>
2018-04-08 15:47:59 -0400

    new test

--- handle.py
+++ handle.py
@@ -16,7 +16,8 @@ if __name__ == '__main__':
     app.run()
     
 """
-import web
+import hashlib
+import webclass
 import receive
 import reply
 
--- reply.py
+++ reply.py
@@ -1,16 +1,21 @@
-# -*- coding: utf-8 -*-# filename: reply.pyimport timeclass Msg(object):
-    def __init__(self):
+# -*- coding: utf-8 -*-
+# filename: reply.py
+import time
+
+class Msg(object):
+      def __init__(self):
         pass
-    def send(self):
+      def send(self):
         return "success"
-    class TextMsg(Msg):
-    def __init__(self, toUserName, fromUserName, content):
+    
+class TextMsg(Msg):
+      def __init__(self, toUserName, fromUserName, content):
         self.__dict = dict()
         self.__dict['ToUserName'] = toUserName
         self.__dict['FromUserName'] = fromUserName
         self.__dict['CreateTime'] = int(time.time())
         self.__dict['Content'] = content
-    def send(self):
+      def send(self):
         XmlForm = """
         <xml>
         <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
@@ -21,14 +26,14 @@
         </xml>
         """
         return XmlForm.format(**self.__dict)
-    class ImageMsg(Msg):
-    def __init__(self, toUserName, fromUserName, mediaId):
+class ImageMsg(Msg):
+      def __init__(self, toUserName, fromUserName, mediaId):
         self.__dict = dict()
         self.__dict['ToUserName'] = toUserName
         self.__dict['FromUserName'] = fromUserName
         self.__dict['CreateTime'] = int(time.time())
         self.__dict['MediaId'] = mediaId
-    def send(self):
+      def send(self):
         XmlForm = """
         <xml>
         <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
