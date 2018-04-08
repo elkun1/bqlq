eb52b89729c36a03a004ace22d0462ad6caa4deb
elkun1 <kun@stanford.edu>
2018-04-07 23:31:07 -0400

    change to handle
    Added original code to handle.py

--- handle.py
+++ handle.py
@@ -6,7 +6,31 @@ import receive
 import web
 
 class Handle(object):
-    def POST(self):
+   def GET(self):
+        try:
+            data = web.input()
+            if len(data) == 0:
+                return "hello, this is handle view"
+            signature = data.signature
+            timestamp = data.timestamp
+            nonce = data.nonce
+            echostr = data.echostr             
+            
+            token = "hellobqlq" #Please specify this value in accordance with the information given in the public platform official site / basic configuration             list = [token, timestamp, nonce]
+            list.sort()
+            sha1 = hashlib.sha1()
+            map(sha1.update, list)
+            hashcode = sha1.hexdigest()
+            print ("handle/GET func: hashcode, signature: ", hashcode, signature)
+            if hashcode == signature:
+                return echostr
+            else:
+                return ""
+        except Exception:
+            return "Exception occurred"
+        
+        
+   def POST(self):
         try:
             webData = web.data()
             print ("Handle Post webdata is ", webData)   #后台打日志
