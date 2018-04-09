5e237c1b604e033a84fe01eca2fca5d88a373cc0
elkun1 <kun@stanford.edu>
2018-04-08 23:24:08 -0400

    new

--- handle.py
+++ handle.py
@@ -44,7 +44,7 @@ class Handle(object):
                     requestText =  "https://api.coinmarketcap.com/v1/ticker/" + recMsg.Content + "/?convert=CNY"
                     response = requests.get(requestText)
                     jsonResponse = json.loads(response.content)
-                    content = '加密货币名称：' str(jsonResponse[0]['name']) + '交易代码：' + str(jsonResponse[0]['symbol']) \
+                    content = '加密货币名称：' + str(jsonResponse[0]['name']) + '交易代码：' + str(jsonResponse[0]['symbol']) \
                     + '交易排名：'  + str(jsonResponse[0]['rank']) \
                     + ' 美元价格：' + str(jsonResponse[0]['price_usd'])  + '美元' + ' 人民币价格：' \
                     + str(jsonResponse[0]['price_cny'])  + '\n' + '人民币' + ' 比特币价格：' \
