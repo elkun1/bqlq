239fa7157c3f1e2a965c2725b1dfc5f7ed01a380
elkun1 <kun@stanford.edu>
2018-04-08 23:29:24 -0400

    new

--- handle.py
+++ handle.py
@@ -49,10 +49,10 @@ class Handle(object):
                     + ' 美元价格：' + str(jsonResponse[0]['price_usd'])  + '美元\n' + ' 人民币价格：' \
                     + str(jsonResponse[0]['price_cny'])  + '人民币' + ' 比特币价格：' \
                     + str(jsonResponse[0]['price_btc']) + '比特币' + ' 全部市值:' + str(jsonResponse[0]['market_cap_usd']) \
-                    + '美元' + ' 24小时交易量：' + str(jsonResponse[0]['24h_volume_usd']) + '美元' + '\n' + ' 市场流通量：' \
+                    + '美元\n' + ' 24小时交易量：' + str(jsonResponse[0]['24h_volume_usd']) + '美元' + ' 市场流通量：' \
                     + str(jsonResponse[0]['available_supply']) + ' 全部流通量：' + str(jsonResponse[0]['total_supply']) \
-                    + ' 1小时价格变动：' + str(jsonResponse[0]['percent_change_1h']) + '%' + ' 24小时价格变动：' \
-                    + str(jsonResponse[0]['percent_change_24h']) + '%\n' + ' 7天价格变动：' + str(jsonResponse[0]['percent_change_7d']) + '%'
+                    + ' 1小时价格变动：' + str(jsonResponse[0]['percent_change_1h']) + '%\n' + ' 24小时价格变动：' \
+                    + str(jsonResponse[0]['percent_change_24h']) + '%' + ' 7天价格变动：' + str(jsonResponse[0]['percent_change_7d']) + '%'
                     replyMsg = reply.TextMsg(toUser, fromUser, content)
                     return replyMsg.send()
                 if recMsg.MsgType == 'image':
