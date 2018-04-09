c60892b9edd839efbca3758f4fcd7207b7325e01
elkun1 <kun@stanford.edu>
2018-04-08 21:10:07 -0400

    new try

--- handle.py
+++ handle.py
@@ -16,12 +16,8 @@ if __name__ == '__main__':
     app.run()
     
 """
-# -*- coding: utf-8 -*- 
 # filename: handle.py
-# coding=utf-8
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+# -*- coding: utf-8 -*- 
 
 import hashlib
 import web
